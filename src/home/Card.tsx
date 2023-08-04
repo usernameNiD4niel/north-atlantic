93abc310213e0a00b0122adf6d40485f1592c210
Daniel Rey <danielrey10252000@gmail.com>
2023-08-04 14:07:59 +0800

    second

--- src/home/Card.tsx
+++ src/home/Card.tsx
@@ -1,11 +1,25 @@
 import { CardTypes } from "../../utils/home";
 
-const Card = ({ transaction, description, color }: CardTypes) => {
+const Card = ({ transaction, description }: CardTypes) => {
 	return (
-		<div className={`bg-[${color}] rounded-lg p-3 w-full`}>
-			<h2 className="text-white text-xl font-bold">{transaction}</h2>
-			<p className="font-thin text-white">{description}</p>
-		</div>
+		<>
+			<div className={`bg-[#0077B6] rounded-lg p-3 w-full`}>
+				<h2 className="text-white text-xl font-bold">{transaction}</h2>
+				<p className="font-thin text-white">{description}</p>
+			</div>
+			<div className={`bg-[#0096C7] rounded-lg p-3 w-full`}>
+				<h2 className="text-white text-xl font-bold">{transaction}</h2>
+				<p className="font-thin text-white">{description}</p>
+			</div>
+			<div className={`bg-[#48CAE4] rounded-lg p-3 w-full`}>
+				<h2 className="text-white text-xl font-bold">{transaction}</h2>
+				<p className="font-thin text-white">{description}</p>
+			</div>
+			<div className={`bg-[#90E0EF] rounded-lg p-3 w-full`}>
+				<h2 className="text-white text-xl font-bold">{transaction}</h2>
+				<p className="font-thin text-white">{description}</p>
+			</div>
+		</>
 	);
 };
 
